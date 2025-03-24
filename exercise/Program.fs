ae20bf7130fe97793afe1d2ad9e03548b70d9c68
權少 <ljqican@163.com>
2025-03-24 20:17:54 +0800

    添加目錄遍歷函數。

--- exercise/Program.fs
+++ exercise/Program.fs
@@ -1,4 +1,6 @@
 ﻿// For more information see https://aka.ms/fsharp-console-apps
+open System.IO
+
 printfn "Hello from F#"
 
 type Color =
@@ -26,3 +28,63 @@ printfn $"green: %A{green}"
 // let d : Derived = Derived()
 
 // d.F()
+
+type Entry =
+    | DirEntry of path: string
+    | FileEntry of path: string
+
+module Path =
+    let getRelativePath relativeTo path =
+        Path.GetRelativePath(relativeTo, path)
+
+module Directory =
+    let enumerateDirectories path =
+        Directory.EnumerateDirectories(path)
+
+    let enumerateFiles path =
+        Directory.EnumerateFiles(path)
+
+    let rec innerTraverse entryList dirResult fileResult=
+        match entryList with
+        | [] -> dirResult, fileResult
+        | entry::rest ->
+            match entry with
+            | DirEntry path ->
+                let dirList =
+                    enumerateDirectories path
+                    |> Seq.map DirEntry
+                    |> Seq.toList
+                let fileList =
+                    enumerateFiles path
+                    |> Seq.map FileEntry
+                    |> Seq.toList
+                let concat a b = List.concat [ a; b ]
+                let rest = concat rest dirList
+                let fileResult = concat fileResult fileList
+                let dirResult = concat dirResult dirList
+                innerTraverse rest dirResult fileResult
+            | FileEntry _ -> dirResult, fileResult
+
+    let exists path =
+        Directory.Exists(path)
+
+    /// <summary>遍历指定路径下的所有目录和文件</summary>
+    /// <param name="path">要遍历的路径</param>
+    /// <returns>返回一个元组，包含目录条目列表和文件条目列表</returns>
+    let traverse path =
+        match exists path with
+        | false -> [], []
+        | true ->
+            innerTraverse [DirEntry(path)] [] []
+
+let entryList path =
+    Directory.traverse path
+
+let dirEntryList, fileEntryList =
+    entryList @"E:\local\fsharp-example\compare\dest"
+
+for i in dirEntryList do
+    printfn $"d: %A{i}"
+
+for i in fileEntryList do
+    printfn $"f: %A{i}"
